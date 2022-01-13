fe0a6087f05456c591881929ec38c29c0923a7ad
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-13 15:58:12 +0530

    3.58

--- src/app/views/Quoteinvoice/Quoteedit.jsx
+++ src/app/views/Quoteinvoice/Quoteedit.jsx
@@ -707,7 +707,8 @@ const InvoiceEditor = ({ isNewInvoice, toggleInvoiceEditor }) => {
     
   
   };
-  const handleSubmit = () => {
+
+  const handleSubmit = (status) => {
     let mode="full"
     updateSidebarMode({ mode })
     // setState({ ...state, ['subTotalCost']: subTotalCost });
@@ -764,6 +765,7 @@ const InvoiceEditor = ({ isNewInvoice, toggleInvoiceEditor }) => {
     formData.append('other',other)
     formData.append('user_id',user.id)
     formData.append('div_id',localStorage.getItem('division'))
+    formData.append('status',status)
     // JSON.stringify(values.rfq_details)
    
     
@@ -786,7 +788,15 @@ const InvoiceEditor = ({ isNewInvoice, toggleInvoiceEditor }) => {
         })
         .then((result) => {
          
-         history.push(navigatePath+"/quoateview/0")
+          if(status=="New")
+          {
+          history.push(navigatePath + "/quote/"+response.data+"/New")
+
+          }
+          else
+          {
+          history.push(navigatePath + "/quoateview/3")
+          }
         // window.location.href="../quoateview"
         })
       })
@@ -1068,7 +1078,7 @@ const InvoiceEditor = ({ isNewInvoice, toggleInvoiceEditor }) => {
           <Icon>arrow_back</Icon>
         </IconButton>
     <div className={clsx("invoice-viewer py-4", classes.invoiceEditor)}>
-      <ValidatorForm onSubmit={handleSubmit} onError={(errors) => null}>
+      <ValidatorForm onSubmit={()=>handleSubmit} onError={(errors) => null}>
         <div className="viewer_actions px-4 flex justify-between">
         <div className="mb-6">
           <h3 align="left"> UPDATE SALES QUOTATION</h3>
@@ -1083,7 +1093,16 @@ const InvoiceEditor = ({ isNewInvoice, toggleInvoiceEditor }) => {
             >
              <Icon>cancel</Icon> CANCEL
             </Button>
-           
+            <Button
+                  type="submit"
+                  className="mr-4 py-2"
+                  variant="outlined"
+                  color="primary"
+                  disabled={loading}
+                  onClick={()=>handleSubmit('draft')}
+                >
+                  <Icon>drafts</Icon> DRAFT
+                </Button>
 
             <Button
               type="submit"
@@ -1091,6 +1110,7 @@ const InvoiceEditor = ({ isNewInvoice, toggleInvoiceEditor }) => {
               variant="outlined"
               color="primary"
               disabled={loading}
+              onClick={()=>handleSubmit('New')}
             >
               <Icon>save</Icon> SAVE & PRINT QUOTATION
             </Button>
