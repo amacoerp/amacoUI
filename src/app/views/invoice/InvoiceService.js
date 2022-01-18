78a973e0b9f3e94dd7324d69ab59a7c1ebda34f8
amacoerp <amacoerp@gmail.com>
2022-01-18 17:14:17 +0530

    5.14PM

--- src/app/views/invoice/InvoiceService.js
+++ src/app/views/invoice/InvoiceService.js
@@ -39,8 +39,8 @@ export const getInvoice = () => {
 };
 
 const url = axios.create({
-  // baseURL: "http://127.0.0.1:8000/api/",
-  baseURL: "http://www.amacoerp.com/test/amaco_test/public/api/",
+  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://www.amacoerp.com/test/amaco_test/public/api/",
   // baseURL: 'http://www.amacoerp.com/amaco/public/api/',
   //  timeout: 1000,
 
