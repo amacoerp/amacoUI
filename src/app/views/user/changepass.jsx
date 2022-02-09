8a17fc554eae0bd960b2db5ac7d1833ae65a9467
amacoerp <amacoerp@gmail.com>
2022-02-09 10:30:09 +0530

    10.30AM

--- src/app/views/user/changepass.jsx
+++ src/app/views/user/changepass.jsx
@@ -26,57 +26,79 @@ const SimpleForm = () => {
     date: new Date(),
   });
   const handleFormSubmit = (event) => {
-    
-    const formdata={
-      id:user.id,
-      password:opassword,
-      newpassword:npassword
+
+    console.log('sds')
+    const formdata = {
+      id: user.id,
+      password: opassword,
+      newpassword: npassword
 
     }
-    
-    url.post("old-password",formdata).then(({ data }) => {
-      
-      if(data.msg===false)
-      {
-        setmessage('Old password is Incorrect')
-      }
-      else
-      {
-        if(npassword!==cpassword)
-        {
-          setmessage('New password doesn\'t match')
-        }
-        else
-        {
-          // const json={
-          //   password:npassword
-          // }
-          // url.put(`/users/${user.id}`,json).then(({ data }) => {
-            // setmessage(null)
-            Swal.fire({
-              title: 'Success',
-              type: 'success',
-              icon: 'success',
-              text: 'Password changed successfully.',
-            });
-            resetform()
 
-          // })
+    if (npassword == cpassword) {
 
+      url.post("old-password-new", formdata).then(({ data }) => {
+        if (data.msg == true) {
+          Swal.fire({
+            title: 'Success',
+            type: 'success',
+            icon: 'success',
+            text: 'Password changed successfully.',
+          });
+          resetform()
+        } else {
+          setmessage('Old password is Incorrect')
         }
-      }
-    })
-};
-const {  user } = useAuth();
-const [opassword, setopassword] = useState('')
-const [npassword, setnpassword] = useState('')
-const [cpassword, setcpassword] = useState('')
-const [message, setmessage] = useState('')
+      })
+
+    } else {
+      setmessage('New password doesn\'t match')
+
+    }
+
+    // url.post("old-password",formdata).then(({ data }) => {
+
+    //   if(data.msg===false)
+    //   {
+    //     setmessage('Old password is Incorrect')
+    //   }
+    //   else
+    //   {
+    //     if(npassword!==cpassword)
+    //     {
+    //       setmessage('New password doesn\'t match')
+    //     }
+    //     else
+    //     {
+    //       // const json={
+    //       //   password:npassword
+    //       // }
+    //       // url.put(`/users/${user.id}`,json).then(({ data }) => {
+    //         // setmessage(null)
+    //         Swal.fire({
+    //           title: 'Success',
+    //           type: 'success',
+    //           icon: 'success',
+    //           text: 'Password changed successfully.',
+    //         });
+    //         resetform()
+
+    //       // })
+
+    //     }
+    //   }
+    // })
+  };
+  const { user } = useAuth();
+  const [opassword, setopassword] = useState('')
+  const [npassword, setnpassword] = useState('')
+  const [cpassword, setcpassword] = useState('')
+  const [message, setmessage] = useState('')
 
 
   // useEffect(() => {
   //   ValidatorForm.addValidationRule("isPasswordMatch", (value) => {
-  
+
 
   //     if (value !== state.password) {
   //       return false;
@@ -86,36 +108,35 @@ const [message, setmessage] = useState('')
   //   return () => ValidatorForm.removeValidationRule("isPasswordMatch");
   // }, [state.password]);
 
-  
+
 
   const handlechange = (event) => {
     setopassword(event.target.value)
-   
-    const formdata={
-      id:user.id,
-      password:opassword
+
+    const formdata = {
+      id: user.id,
+      password: opassword
     }
-    url.get("/old-password",formdata).then(({ data }) => {
-     
-      if(!data)
-      {
+    url.get("/old-password", formdata).then(({ data }) => {
+
+      if (!data) {
         setmessage('Old password is Incorrect')
       }
 
     })
-}
+  }
 
   const handleDateChange = (date) => {
     setState({ ...state, date });
   };
-  const resetform =()=>{
+  const resetform = () => {
     setnpassword('')
     setcpassword('')
     setopassword('')
     setmessage('')
 
   }
-  
+
 
   const {
     username,
@@ -132,11 +153,11 @@ const [message, setmessage] = useState('')
   return (
     <div>
       <Card className="m-sm-30 p-25" elevation={20}>
-      <div className="max-w-600 mx-auto">
-   
-            <ValidatorForm onSubmit={handleFormSubmit} autoComplete="off" width="200px">
-      
-      <TextField
+        <div className="max-w-600 mx-auto">
+
+          <ValidatorForm onSubmit={handleFormSubmit} autoComplete="off" width="200px">
+
+            <TextField
               className="w-full mb-4"
               label="Old Password"
               variant="outlined"
@@ -145,11 +166,11 @@ const [message, setmessage] = useState('')
               name="name"
               size="small"
               value={opassword}
-             
-              
+
+
             />
-         
-            
+
+
             <TextField
               className="w-full mb-4"
               label="New Password"
@@ -161,39 +182,39 @@ const [message, setmessage] = useState('')
               name="cname"
               size="small"
               value={npassword}
-             
-              
+
+
             />
-            
-                 <TextField
-                             className="w-full mb-4"
-                              label="Confirm Password"
-                              autoComplete="none"
-                              onChange={e => setcpassword(e.target.value)}
-                              name="mobno"
-                              type="text"
-                              size="small"
-                              variant="outlined"
-                              value={cpassword}
-                              fullWidth
-                                                            
-                          />
-                          
-                {message && <p className="text-error">{message}</p>}
-            
-        
-        <div className="flex items-center ">
-          <Button variant="outlined" color="primary" type="submit" >
-           <Icon>save</Icon> Save
-          </Button>
-          <Button color=".bg-green" variant="outlined"  className="ml-4" onClick={resetform}>
-        <Icon>loop</Icon>
-          <span className="pl-2 capitalize">reset</span>
-        </Button>
-          <div className="flex justify-between items-center">
-          
-          
-          {/* <Button
+
+            <TextField
+              className="w-full mb-4"
+              label="Confirm Password"
+              autoComplete="none"
+              onChange={e => setcpassword(e.target.value)}
+              name="mobno"
+              type="text"
+              size="small"
+              variant="outlined"
+              value={cpassword}
+              fullWidth
+
+            />
+
+            {message && <p className="text-error">{message}</p>}
+
+
+            <div className="flex items-center ">
+              <Button variant="outlined" color="primary" type="submit" >
+                <Icon>save</Icon> Save
+              </Button>
+              <Button color=".bg-green" variant="outlined" className="ml-4" onClick={resetform}>
+                <Icon>loop</Icon>
+                <span className="pl-2 capitalize">reset</span>
+              </Button>
+              <div className="flex justify-between items-center">
+
+
+                {/* <Button
           
             variant="outlined"
             color="primary"
@@ -201,16 +222,16 @@ const [message, setmessage] = useState('')
           >
             <Icon color="primary">remove_red_eye</Icon>view
           </Button> */}
-          </div>
-        </div>
-        
-      </ValidatorForm>
-      </div>
-      </Card>
-      </div>
-  
+              </div>
+            </div>
 
-  
+          </ValidatorForm>
+        </div>
+      </Card>
+    </div>
+
+
+
   );
 };
 
