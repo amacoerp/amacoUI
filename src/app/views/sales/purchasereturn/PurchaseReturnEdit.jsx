bdf85792783b4c71999225f99e63cd1334d7c8c9
amacoerp <deekshitha.cse5235@gmail.com>
2022-02-12 10:39:36 +0530

    10.39

--- src/app/views/sales/purchasereturn/PurchaseReturnEdit.jsx
+++ src/app/views/sales/purchasereturn/PurchaseReturnEdit.jsx
@@ -442,7 +442,7 @@ const GenPurchaseReturn = ({ isNewInvoice, toggleInvoiceEditor }) => {
         arr.quotation_details = tempItemList
         arr.discount_in_p = 0
         arr.total_value = parseFloat(subTotalCost).toFixed(2)
-        arr.net_amount = total
+        arr.net_amount = GTotal
         arr.freight = freight
         arr.vat_in_value = parseFloat(charge).toFixed(2)
         arr.rfq_id = id
