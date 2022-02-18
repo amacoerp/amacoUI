40d0e5ec91a1c2cdbb671c6f8254d71977c19110
amacoerp <amacoerp@gmail.com>
2022-02-18 11:10:45 +0530

    11.10

--- src/app/views/Stock/Stock.jsx
+++ src/app/views/Stock/Stock.jsx
@@ -34,6 +34,8 @@ const StockViewer = () => {
     const [shouldOpenViewDialog, setShouldOpenViewDialog] = useState(false);
     const [userList, setUserList] = useState([]);
     const [dataList, setDataList] = useState([]);
+    const [sl, setSl] = useState('')
+    const [other, setOther] = useState([])
     const [userid, setuserid] = useState(null);
     const [did, setdlid] = useState('');
     const [uid, setUid] = useState('');
@@ -72,18 +74,9 @@ const StockViewer = () => {
     useEffect(() => {
 
         url.get("stock").then(({ data }) => {
-            const arr = data.map((item) => {
-                item['sum_purchase_qty'] = item.product_purchase?.reduce((obj, val) => obj + parseFloat(val?.quantity), 0)
-
-                item['sum_sales_qty'] = item?.product_sales?.reduce((obj, val) => obj + parseFloat(val?.quantity), 0)
-                item['initial_qty'] = console.log(parseFloat(item?.initial_quantity))
-                item['sum_purchase_return_qty'] = item.purchase_sale__return?.filter(objs => objs.po_number != '').reduce((obj, val) => obj + parseFloat(val.quantity), 0)
-                item['sum_sales_return_qty'] = item.purchase_sale__return?.filter(objs => objs.po_number == '').reduce((obj, val) => obj + parseFloat(val.quantity), 0)
-                return item;
-            })
-
-            setDataList(arr)
-            console.log(arr)
+            setDataList(data.category)
+            setOther(data.other)
+            console.log(data)
         });
 
 
@@ -104,46 +97,85 @@ const StockViewer = () => {
 
                 </div>
             </div>
-            <Card className="mb-4" style={{ padding: '10px' }} elevation={0} borderRadius="borderRadius" >
+            <Card className="mb-4" style={{ padding: '20px' }} elevation={0} borderRadius="borderRadius" >
 
-                <div className="row " >
-                    <div className="col slHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>S.NO.</div>
-                    <div className="col catHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>CATEGORY</div>
-                    <div className="col subHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>SUB CATEGORY</div>
-                    <div className="col prodHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>PRODUCT</div>
-                    <div className="col priceHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>PRICE</div>
-                    <div className="col stockHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>STOCK</div>
-                    <div className="col stockValue" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>STOCK VALUE</div>
-                </div>
-                <div className="">
-                    {dataList?.map((item, i) => {
-                        return <div className="row" key={i}>
-                            <div className="col slno">
-                                {++i}
+                <div style={{ position: 'relative', left: 20 }}>
+                    <div className="row " >
+                        <div className="col slHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>S.NO.</div>
+                        <div className="col catHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>CATEGORY</div>
+                        <div className="col subHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>SUB CATEGORY</div>
+                        <div className="col prodHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>PRODUCT</div>
+                        <div className="col priceHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>PRICE</div>
+                        <div className="col stockHead" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>STOCK</div>
+                        <div className="col stockValue" style={{ border: "1px solid #ccc", fontFamily: "Calibri", fontWeight: '1000', backgroundColor: '#1d2257', color: 'white', fontSize: 16, padding: 10, textAlign: 'center' }}>STOCK VALUE</div>
+                    </div>
+                    <div className="">
+                        {dataList?.map((item, i) => {
+
+                            return <div className="row" key={i}>
+                                <div className="col slno">
+                                    {++i}
+                                </div>
+                                <div className="col capitalize catCol">{item?.name}</div>
+                                {item?.product_category.length ? <div className="col">{item?.product_category?.map((subItem, si) => {
+                                    return <>
+                                        <div className="row" key={si}>
+                                            <div className="col capitalize subCol">{subItem?.name}</div>
+                                            {subItem?.product.length ? <div className="col">{subItem?.product?.map((prod, pi) => {
+                                                const sum = ((parseInt(prod?.purchaseQuantity) + parseInt(prod?.salesReturnQuantity) + parseInt(prod?.initial_quantity)) + (parseInt(prod?.salesQuantity) - parseInt(prod?.purchaseReturnQuantity)));
+                                                return <>
+                                                    <div className="row" key={pi}>
+                                                        <div className="col prodCol">{prod.name}</div>
+                                                        <div className="col priceCol">{parseFloat(prod?.latestPrice[0]?.purchase_price) ? parseFloat(prod?.latestPrice[0]?.purchase_price) : 0}</div>
+                                                        <div className="col stockCol">{sum}</div>
+                                                        <div className="col stockVCol">{(sum * (parseFloat(prod?.latestPrice[0]?.purchase_price) ? parseFloat(prod?.latestPrice[0]?.purchase_price) : 0))}</div>
+                                                    </div>
+                                                </>
+                                            })}</div> :
+                                                <>
+                                                    <div className="col prodCol">--</div>
+                                                    <div className="col priceCol">--</div>
+                                                    <div className="col stockCol">--</div>
+                                                    <div className="col stockVCol">--</div>
+                                                </>
+                                            }
+
+                                        </div>
+
+                                    </>
+                                })}</div> :
+                                    <>
+                                        <div className="col subCol">--</div>
+                                        <div className="col prodCol">--</div>
+                                        <div className="col priceCol">--</div>
+                                        <div className="col stockCol">--</div>
+                                        <div className="col stockVCol">--</div>
+                                    </>
+                                }
+
                             </div>
-                            <div className="col capitalize catCol">{item?.name}</div>
-                            <div className="col">{item?.product_category?.map((subItem, si) => {
-                                return <>
-                                    <div className="row" key={si}>
-                                        <div className="col capitalize subCol">{subItem?.name}</div>
-                                        <div className="col">{subItem?.product?.map((prod, pi) => {
-                                            const sum = ((parseInt(prod?.purchaseQuantity) + parseInt(prod?.salesReturnQuantity) + parseInt(prod?.initial_quantity)) + (parseInt(prod?.salesQuantity) - parseInt(prod?.purchaseReturnQuantity)));
-                                            return <>
-                                                <div className="row" key={pi}>
-                                                    <div className="col prodCol">{prod.name}</div>
-                                                    <div className="col priceHead">{parseFloat(prod?.latestPrice[0]?.purchase_price) ? parseFloat(prod?.latestPrice[0]?.purchase_price) : 0}</div>
-                                                    <div className="col stockHead">{sum}</div>
-                                                    <div className="col stockValue">{(sum * (parseFloat(prod?.latestPrice[0]?.purchase_price) ? parseFloat(prod?.latestPrice[0]?.purchase_price) : 0))}</div>
-                                                </div>
-                                            </>
-                                        })}</div>
+
+                        })}
+                        {other?.map((prod, i) => {
+                            const sum = ((parseInt(prod?.purchaseQuantity) + parseInt(prod?.salesReturnQuantity) + (prod?.initial_quantity == null ? 0 : parseInt(prod?.initial_quantity))) + (parseInt(prod?.salesQuantity) - parseInt(prod?.purchaseReturnQuantity)));
+
+                            return <>
+                                <div className="row">
+                                    <div className="col slno">{(parseInt(dataList.length) + (++i))}</div>
+                                    <div className="col catCol">--</div>
+                                    <div className="col subCol">--</div>
+                                    <div className="col prodCol">
+                                        {prod.name}
                                     </div>
+                                    <div className="col priceCol">{parseFloat(prod?.latestPrice[0]?.purchase_price) ? parseFloat(prod?.latestPrice[0]?.purchase_price) : 0}</div>
+                                    <div className="col stockCol">{sum}</div>
+                                    <div className="col stockVCol">{(sum * (parseFloat(prod?.latestPrice[0]?.purchase_price) ? parseFloat(prod?.latestPrice[0]?.purchase_price) : 0))}</div>
 
-                                </>
-                            })}</div>
-                        </div>
+                                </div>
 
-                    })}
+                            </>
+                        })}
+                    </div>
                 </div>
                 {/* 
                 <br />
