1cac30f7763f29ca07831516d718c6db11ad5e90
amacoerp <amacoerp@gmail.com>
2022-02-22 16:39:57 +0530

    4.23

--- src/app/views/Stock/Stock.jsx
+++ src/app/views/Stock/Stock.jsx
@@ -124,6 +124,7 @@ const StockViewer = () => {
                 </div>
             </div>
             <Card className="mb-4" style={{ padding: '20px' }} elevation={0} borderRadius="borderRadius" >
+                {console.log('ds')}
                 <Button
                     variant="outlined"
                     color="primary"
