ec3a90eceda2d6e6c0bbd26c20345cf567818a97
amacoerp <deekshitha.cse5235@gmail.com>
2022-02-26 10:45:34 +0530

    10.45

--- build1/static/js/248.0ab1e779.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpmatx-react"]=this["webpackJsonpmatx-react"]||[]).push([[248],{3364:function(t,a){}}]);
--- build2/static/js/247.60b0f39f.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpmatx-react"]=this["webpackJsonpmatx-react"]||[]).push([[247],{3359:function(t,a){}}]);
