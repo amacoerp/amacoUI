aafccb856fc8bd128e1389bfbcfd81ceaf3413ee
amacoerp <deekshitha.cse5235@gmail.com>
2022-03-07 15:37:48 +0530

    3.37

--- src/app/views/invoice/InvoiceService.js
+++ src/app/views/invoice/InvoiceService.js
@@ -39,8 +39,8 @@ export const getInvoice = () => {
 };
 
 const url = axios.create({
- baseURL: "http://127.0.0.1:8000/api/",
-  // baseURL: "http://www.amacoerp.com/test/amaco_test/public/api/",
+baseURL: "http://127.0.0.1:8000/api/",
+  //  baseURL: "http://www.amacoerp.com/test/amaco_test/public/api/",
   //  baseURL: "http://18.210.239.86/amaco/public/api/",
   // baseURL: "http://www.amacoerp.com/amaco/public/api/",
   //  timeout: 1000,
@@ -48,7 +48,7 @@ const url = axios.create({
 });
   export const basePath = "http://127.0.0.1:8000/api";
     // export const basePath = "http://18.210.239.86/amaco/";
-  //  export const basePath = "http://www.amacoerp.com/test/amaco_test/";
+  // export const basePath = "http://www.amacoerp.com/test/amaco_test/";
       // export const basePath = "http://www.amacoerp.com/amaco/";
 //   const url = axios.create({
 //     baseURL: 'http://www.amacoerp.com/amaco/public/api/',
@@ -65,8 +65,8 @@ export const GDIV = localStorage.getItem("division")
   ? localStorage.getItem("division")
   : 1;
 
-export const version = "";
-// export const version = "Test Version";
+// export const version = "";
+export const version = "Test Version";
 
 export const ApiKey = "";
 const role = localStorage.getItem("role");
