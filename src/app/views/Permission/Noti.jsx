d9bbfba0bb96a1a81264ca8d5dbf30dc4b7cf2b7
Pradeep <pradeephegde463@gmail.com>
2022-03-28 17:42:19 +0530

    150

--- src/app/views/Permission/Noti.jsx
+++ src/app/views/Permission/Noti.jsx
@@ -1,20 +1,29 @@
+// import React from 'react'
+// import { useEffect } from 'react';
+// import Pusher from 'pusher-js';
+
+// export default function Noti() {
+
+//     useEffect(()=>{
+//         var pusher = new Pusher("Avhcqfuedh6BGPXVRyXJtjPIoxjgUqMELSwTlbsj5OY=", {
+//             cluster: "ap2",
+//             encrypted: true,
+//           });
+     
+//           var channel = pusher.subscribe("notifyChannel");
+//            channel.bind("notifyChannel", async function (response) {
+//                alert('some notification');
+//            })
+//     },[])
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
 import React from 'react'
-import { useEffect } from 'react';
-import Pusher from 'pusher-js';
 
 export default function Noti() {
-
-    useEffect(()=>{
-        var pusher = new Pusher("Avhcqfuedh6BGPXVRyXJtjPIoxjgUqMELSwTlbsj5OY=", {
-            cluster: "ap2",
-            encrypted: true,
-          });
-     
-          var channel = pusher.subscribe("notifyChannel");
-           channel.bind("notifyChannel", async function (response) {
-               alert('some notification');
-           })
-    },[])
   return (
     <div>
       
