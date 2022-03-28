4ca89119797c13cbf7073de1c18f2e9decfb39c6
Pradeep <pradeephegde463@gmail.com>
2022-03-28 14:53:20 +0530

    150

--- src/app/views/Permission/Noti.jsx
+++ src/app/views/Permission/Noti.jsx
@@ -1,38 +1,20 @@
 import React from 'react'
-import Echo from 'laravel-echo';
-
+import { useEffect } from 'react';
+import Pusher from 'pusher-js';
 
 export default function Noti() {
-   
- 
-window.Pusher = require('pusher-js');
- 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'Avhcqfuedh6BGPXVRyXJtjPIoxjgUqMELSwTlbsj5OY=',
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true
-});
 
-const options = {
-    broadcaster: 'pusher',
-    key: 'Avhcqfuedh6BGPXVRyXJtjPIoxjgUqMELSwTlbsj5OY=',
-    // cluster: config.pusher.cluster,
-    forceTLS: true,
-    // authEndpoint is your apiUrl + /broadcasting/auth
-    authEndpoint: 'http://127.0.0.1:8000/api/noti', 
-    // As I'm using JWT tokens, I need to manually set up the headers.
-    auth: {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        Accept: 'application/json',
-      },
-    },
-  };
-const echo = new Echo(options);
-echo.private(`App.User.${localStorage.getItem('user_id')}`).notification((data) => {
-    console.log(data);
-});
+    useEffect(()=>{
+        var pusher = new Pusher("Avhcqfuedh6BGPXVRyXJtjPIoxjgUqMELSwTlbsj5OY=", {
+            cluster: "ap2",
+            encrypted: true,
+          });
+     
+          var channel = pusher.subscribe("notifyChannel");
+           channel.bind("notifyChannel", async function (response) {
+               alert('some notification');
+           })
+    },[])
   return (
     <div>
       
